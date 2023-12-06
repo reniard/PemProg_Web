72985759a4499af19ba40f1fbfbeed14fde1fa0a
reniard <115964932+reniard@users.noreply.github.com>
2023-12-06 19:48:15 +0700

    Add files via upload

---
+++ pertemuan_9/tampil.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+
+</head>
+<body> 
+<h2> Tampilan Form Handling </h2>
+NIM : <?php echo $_POST["nim"];?><br>
+Nama : <?php echo $_POST["nama"];?><br>
+Alamat : <?php echo $_POST["alamat"];?><br>
+</body>
+
+</html>
---
+++ pertemuan_9/tampil_get.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+
+</head>
+<body> 
+<h2> Tampilan Form Handling </h2>
+NIM : <?php echo $_GET["nim"];?><br>
+Nama : <?php echo $_GET["nama"];?><br>
+Alamat : <?php echo $_GET["alamat"];?><br>
+</body>
+
+</html>
